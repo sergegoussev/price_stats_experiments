0ca0067fd78f782b25429fa7e28e74e4711ec9f7
Serge Goussev <goussev.serge@gmail.com>
2024-10-02 22:11:13 -0400

    cleanup of aizcorbe 2.1. notebook

---
+++ src/nipa_helper.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def clean_nipa_data():
+    """
+    Cleans the NIPA data used in Aizcorbe's example 2.1
+
+    Takes two separate csv's in bronze and creates an analysis ready
+    dataset
+    """
+    
+
